bd14d2be06389e18de038aead45872c240cc2ff8
NMSAzulX <16236903+NMSAzulX@users.noreply.github.com>
2019-07-17 18:26:27 +0800

    upload project

---
+++ TypeTest/Program.cs
@@ -0,0 +1,23 @@
+﻿using BenchmarkDotNet.Running;
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Runtime.Loader;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+
+namespace TypeTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            BenchmarkRunner.Run<TestRun>();
+            Console.ReadKey();
+
+        }
+    }
+
+}
---
+++ TypeTest/TestRun.cs
@@ -0,0 +1,66 @@
+﻿using BenchmarkDotNet.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Reflection.Emit;
+using System.Text;
+
+namespace TypeTest
+{
+    public class TestRun
+    {
+
+        public FieldInfo NameInfo = typeof(TestBechmark).GetField("Name");
+        public readonly Action<TestBechmark, string> EmitSetString;
+        public TestRun()
+        {
+            DynamicMethod method = new DynamicMethod("GetString", null, new Type[] { typeof(TestBechmark), typeof(string) });
+            ILGenerator il = method.GetILGenerator();
+            il.Emit(OpCodes.Ldarg_0);
+            il.Emit(OpCodes.Ldarg_1);
+            il.Emit(OpCodes.Stfld, typeof(TestBechmark).GetField("Name"));
+            il.Emit(OpCodes.Ret);
+            EmitSetString = (Action<TestBechmark, string>)(method.CreateDelegate(typeof(Action<TestBechmark, string>)));
+        }
+
+        [Benchmark]
+        public void Origin()
+        {
+
+            TestBechmark instance = new TestBechmark();
+            instance.Name = "123456789";
+
+        }
+
+        [Benchmark]
+        public void Dynamic()
+        {
+            TestBechmark instance = new TestBechmark();
+            RunDynamic(instance);
+        }
+
+        [Benchmark]
+        public void Reflect()
+        {
+            TestBechmark instance = new TestBechmark();
+            NameInfo.SetValue(instance, "123456789");
+        }
+
+        [Benchmark]
+        public void Emit()
+        {
+            TestBechmark instance = new TestBechmark();
+            EmitSetString(instance, "123456789");
+        }
+
+        public void RunDynamic(dynamic instance)
+        {
+            instance.Name = "123456789";
+        }
+    }
+
+    public class TestBechmark
+    {
+        public string Name;
+    }
+}
